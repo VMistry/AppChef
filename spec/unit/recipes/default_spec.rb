35d2978d6f6f20396fe9bb3816f510710fa06aaa
VMistry <virenmistry@sky.com>
2018-09-24 15:49:55 +0100

    AppChef finished

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -30,7 +30,12 @@ describe 'node::default' do
     it "should install pm2 via npm" do
       expect(chef_run).to install_nodejs_npm("pm2")
     end
+
+    it 'should create a proxy.conf template in /etc/nginx/sites_available' do
+      expect(chef_run).to create_template("/etc/nginx/sites_available/proxy.conf")
+    end
   end
+
 
 
 
